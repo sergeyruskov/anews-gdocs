316351b530bfc66610866b2bc15dfb2503f54c3b
Your Name <sergeyruskov@gmail.com>
2017-12-19 18:17:42 +0300

    [FIX]

--- server/index.js
+++ server/index.js
@@ -93,10 +93,12 @@ export const surroundSelectionText = () => {
 			const completedText = `${start}${text}${end}`;
 			el.setText('');
 
+			paragraph.appendText(start);
 			paragraph.appendHorizontalRule();
-			paragraph.appendText(completedText);
+			paragraph.appendText(text);
 
 			paragraph.appendHorizontalRule();
+			paragraph.appendText(end);
 
 			const style = {};
 			style[DocumentApp.Attribute.HORIZONTAL_ALIGNMENT] =
